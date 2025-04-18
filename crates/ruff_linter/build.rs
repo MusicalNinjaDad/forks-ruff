ff82841deb249ad97a52661e0c939a88b060afa4
Mike Foster <i.am.mike.foster@gmail.com>
2025-04-18 18:04:38 +0000

    rename cfg flag: build.rs has no access to #[cfg(test)]

--- crates/ruff_linter/build.rs
+++ crates/ruff_linter/build.rs
@@ -1,11 +1,8 @@
 fn main() {
     // Allow `#[cfg(wsl)]` to be used to select specific tests for WSL
     // This is needed to validate special handling of EXE001 & EXE002
-    println!("cargo::rustc-check-cfg=cfg(wsl)");
-    
-    // Set the `wsl` cfg flag for tests (only)
-    #[cfg(test)]
+    println!("cargo::rustc-check-cfg=cfg(testing_on_wsl)");
     if is_wsl::is_wsl() {
-        println!("cargo::rustc-cfg=wsl");
+        println!("cargo::rustc-cfg=testing_on_wsl");
     }
 }
