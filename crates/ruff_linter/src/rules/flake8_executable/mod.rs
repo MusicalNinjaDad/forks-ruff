7734bdeec7f7944ba654eb3f388564123c0c23bf
Mike Foster <i.am.mike.foster@gmail.com>
2025-04-18 17:07:43 +0000

    run EXE003...EXE005 on wsl

--- crates/ruff_linter/src/rules/flake8_executable/mod.rs
+++ crates/ruff_linter/src/rules/flake8_executable/mod.rs
@@ -45,4 +45,28 @@ mod tests {
         assert_messages!(snapshot, diagnostics);
         Ok(())
     }
+
+    #[test_case(Path::new("EXE003.py"))]
+    #[test_case(Path::new("EXE003_uv.py"))]
+    #[test_case(Path::new("EXE004_1.py"))]
+    #[test_case(Path::new("EXE004_2.py"))]
+    #[test_case(Path::new("EXE004_3.py"))]
+    #[test_case(Path::new("EXE004_4.py"))]
+    #[test_case(Path::new("EXE005_1.py"))]
+    #[test_case(Path::new("EXE005_2.py"))]
+    #[test_case(Path::new("EXE005_3.py"))]
+    #[cfg(wsl)]
+    fn rules_wsl(path: &Path) -> Result<()> {
+        let snapshot = path.to_string_lossy().into_owned();
+        let diagnostics = test_path(
+            Path::new("flake8_executable").join(path).as_path(),
+            &settings::LinterSettings::for_rules(vec![
+                Rule::ShebangLeadingWhitespace,
+                Rule::ShebangNotFirstLine,
+                Rule::ShebangMissingPython,
+            ]),
+        )?;
+        assert_messages!(snapshot, diagnostics);
+        Ok(())
+    }
 }
