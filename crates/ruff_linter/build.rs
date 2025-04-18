8d132af064628d78592d2ef628fe1a4bb744c66c
Mike Foster <i.am.mike.foster@gmail.com>
2025-04-18 17:21:21 +0000

    auto-detect wsl when running tests

--- crates/ruff_linter/build.rs
+++ crates/ruff_linter/build.rs
@@ -1,7 +1,11 @@
 fn main() {
-    // Allow cfg(wsl) to be used to select specific tests for WSL
+    // Allow `#[cfg(wsl)]` to be used to select specific tests for WSL
     // This is needed to validate special handling of EXE001 & EXE002
-    // Not calculated - to avoid dev dependency on is_wsl
-    // Use `RUSTFLAGS="--cfg wsl" cargo insta ...`
     println!("cargo::rustc-check-cfg=cfg(wsl)");
+    
+    // Set the `wsl` cfg flag for tests (only)
+    #[cfg(test)]
+    if is_wsl::is_wsl() {
+        println!("cargo::rustc-cfg=wsl");
+    }
 }
