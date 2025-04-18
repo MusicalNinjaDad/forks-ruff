2e6a1b17a0499ec33097c605f208487e45a40651
Mike Foster <i.am.mike.foster@gmail.com>
2025-04-18 19:04:35 +0000

    run non-wsl tests if RUFF_WSL_FILESYSTEM="ext4"

--- crates/ruff_linter/build.rs
+++ crates/ruff_linter/build.rs
@@ -3,6 +3,10 @@ fn main() {
     // This is needed to validate special handling of EXE001 & EXE002
     println!("cargo::rustc-check-cfg=cfg(testing_on_wsl)");
     if is_wsl::is_wsl() {
-        println!("cargo::rustc-cfg=testing_on_wsl");
+        // The RUFF_WSL_FILESYSTEM environment variable overrides WSL handling
+        // If set when testing: run non-wsl tests
+        if option_env!("RUFF_WSL_FILESYSTEM") != Some("ext4")  {
+            println!("cargo::rustc-cfg=testing_on_wsl");
+        }
     }
 }
