6c177a3c88a90cbb403c8aaaf4a174ea93e29f6f
Mike Foster <i.am.mike.foster@gmail.com>
2025-04-19 15:50:08 +0000

    dry value name

--- crates/ruff_linter/build.rs
+++ crates/ruff_linter/build.rs
@@ -2,7 +2,7 @@ fn main() {
     // Allow `#[cfg(test_environment="wsl-ntfs")]` to be used to select specific tests for WSL
     // This is needed to validate special handling of EXE001 & EXE002
     println!("cargo::rustc-check-cfg=cfg(test_environment, values(\"wsl-ntfs\"))");
-    if option_env!("RUFF_TEST_ENVIRONMENT") == Some("wsl-ntfs")  {
-            println!("cargo::rustc-cfg=test_environment=\"wsl-ntfs\"");
+    if let Some(value) = option_env!("RUFF_TEST_ENVIRONMENT") {
+            println!("cargo::rustc-cfg=test_environment=\"{value}\"");
         }
 }
