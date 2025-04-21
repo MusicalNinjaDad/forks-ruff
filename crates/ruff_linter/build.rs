e775c653308fff2a7581cf3fc52148cf57a9c58a
Mike Foster <i.am.mike.foster@gmail.com>
2025-04-21 10:26:52 +0000

    simplify tests

--- crates/ruff_linter/build.rs
+++ crates/ruff_linter/build.rs
@@ -1,8 +1,13 @@
 fn main() {
-    // Allow e.g. `#[cfg(test_environment="wsl:ntfs")]` to be used to select specific tests for WSL
-    // This is needed to validate special handling of EXE001 & EXE002
+    // Allow e.g. `#[cfg(test_environment="ntfs")]` to be used to select specific tests to run on different environments,
+    // when the environment cannot be easily identified with standard cfg flags
+    //
+    // This is currently used to validate special handling of EXE001 & EXE002 on mounted NTFS filesystems
+    //
+    // Set `RUFF_TEST_ENVIRONMENT` before running tests, to specify the execution environment
     // Format is a list of values, separated using the OS-specific path separator (`:` on WSL/*nix, ";" on Windows)
-    println!("cargo::rustc-check-cfg=cfg(test_environment, values(\"wsl\",\"ntfs\",\"ruff_wsl_filesystem_is_set\"))");
+    // e.g. `RUFF_TEST_ENVIRONMENT="ntfs" cargo insta ...`
+    println!("cargo::rustc-check-cfg=cfg(test_environment, values(\"ntfs\"))");
     if let Some(list_of_values) = option_env!("RUFF_TEST_ENVIRONMENT") {
         for value in std::env::split_paths(list_of_values) {
             println!(
--- crates/ruff_linter/src/rules/flake8_executable/mod.rs
+++ crates/ruff_linter/src/rules/flake8_executable/mod.rs
@@ -14,10 +14,12 @@ mod tests {
     use crate::test::test_path;
     use crate::{assert_messages, settings};
 
-    #[test_case(Path::new("EXE001_1.py"))]
+    #[cfg_attr(not(test_environment = "ntfs"), test_case(Path::new("EXE001_1.py")))]
+    #[cfg_attr(test_environment = "ntfs", test_case(Path::new("EXE001_1_wsl.py")))]
     #[test_case(Path::new("EXE001_2.py"))]
     #[test_case(Path::new("EXE001_3.py"))]
-    #[test_case(Path::new("EXE002_1.py"))]
+    #[cfg_attr(not(test_environment = "ntfs"), test_case(Path::new("EXE002_1.py")))]
+    #[cfg_attr(test_environment = "ntfs", test_case(Path::new("EXE002_1_wsl.py")))]
     #[test_case(Path::new("EXE002_2.py"))]
     #[test_case(Path::new("EXE002_3.py"))]
     #[test_case(Path::new("EXE003.py"))]
@@ -31,112 +33,6 @@ mod tests {
     #[test_case(Path::new("EXE005_3.py"))]
     #[cfg(not(test_environment = "ntfs"))]
     fn rules(path: &Path) -> Result<()> {
-        let snapshot = path.to_string_lossy().into_owned();
-        let diagnostics = test_path(
-            Path::new("flake8_executable").join(path).as_path(),
-            &settings::LinterSettings::for_rules(vec![
-                Rule::ShebangNotExecutable,
-                Rule::ShebangMissingExecutableFile,
-                Rule::ShebangLeadingWhitespace,
-                Rule::ShebangNotFirstLine,
-                Rule::ShebangMissingPython,
-            ]),
-        )?;
-        assert_messages!(snapshot, diagnostics);
-        Ok(())
-    }
-
-    #[test_case(Path::new("EXE001_1.py"))]
-    #[test_case(Path::new("EXE002_1.py"))]
-    #[cfg(
-        any(
-            not(test_environment="wsl"), // OR
-            test_environment="ruff_wsl_filesystem_is_set"
-        )
-    )]
-    fn no_warnings(path: &Path) -> Result<()> {
-        testing_logger::setup();
-        test_path(
-            Path::new("flake8_executable").join(path).as_path(),
-            &settings::LinterSettings::for_rules(vec![
-                Rule::ShebangNotExecutable,
-                Rule::ShebangMissingExecutableFile,
-            ]),
-        )?;
-        testing_logger::validate(|captured_logs| {
-            assert_eq!(captured_logs.len(), 0);
-        });
-        Ok(())
-    }
-
-    #[test_case(Path::new("EXE001_1.py"))]
-    #[test_case(Path::new("EXE002_1.py"))]
-    #[cfg(
-        all(
-            test_environment="ntfs", // AND
-            not(test_environment="ruff_wsl_filesystem_is_set")
-        )
-    )]
-    fn warnings_ntfs(path: &Path) -> Result<()> {
-        testing_logger::setup();
-        test_path(
-            Path::new("flake8_executable").join(path).as_path(),
-            &settings::LinterSettings::for_rules(vec![
-                Rule::ShebangNotExecutable,
-                Rule::ShebangMissingExecutableFile,
-            ]),
-        )?;
-        testing_logger::validate(|captured_logs| {
-            assert_eq!(captured_logs.len(), 1);
-            assert_eq!(captured_logs[0].body, "EXE001/EXE002 is not available on WSL when a windows filesystem is mounted - see the docs for more information.");
-            assert_eq!(captured_logs[0].level, log::Level::Warn);
-        });
-        Ok(())
-    }
-
-    #[test_case(Path::new("EXE001_1.py"))]
-    #[test_case(Path::new("EXE002_1.py"))]
-    #[cfg(
-        all(
-            test_environment="wsl", // AND
-            not(test_environment="ntfs"), // AND
-            not(test_environment="ruff_wsl_filesystem_is_set")
-        )
-    )]
-    fn warnings_wsl_ext4_detected(path: &Path) -> Result<()> {
-        testing_logger::setup();
-        test_path(
-            Path::new("flake8_executable").join(path).as_path(),
-            &settings::LinterSettings::for_rules(vec![
-                Rule::ShebangNotExecutable,
-                Rule::ShebangMissingExecutableFile,
-            ]),
-        )?;
-        testing_logger::validate(|captured_logs| {
-            assert_eq!(captured_logs.len(), 1);
-            assert_eq!(captured_logs[0].body, "EXE001/EXE002 incur a small performance hit on WSL unless RUFF_WSL_FILESYSTEM is set - see the docs for more information.");
-            assert_eq!(captured_logs[0].level, log::Level::Warn);
-        });
-        Ok(())
-    }
-
-    #[test_case(Path::new("EXE001_1_wsl.py"))]
-    #[test_case(Path::new("EXE001_2.py"))]
-    #[test_case(Path::new("EXE001_3.py"))]
-    #[test_case(Path::new("EXE002_1_wsl.py"))]
-    #[test_case(Path::new("EXE002_2.py"))]
-    #[test_case(Path::new("EXE002_3.py"))]
-    #[test_case(Path::new("EXE003.py"))]
-    #[test_case(Path::new("EXE003_uv.py"))]
-    #[test_case(Path::new("EXE004_1.py"))]
-    #[test_case(Path::new("EXE004_2.py"))]
-    #[test_case(Path::new("EXE004_3.py"))]
-    #[test_case(Path::new("EXE004_4.py"))]
-    #[test_case(Path::new("EXE005_1.py"))]
-    #[test_case(Path::new("EXE005_2.py"))]
-    #[test_case(Path::new("EXE005_3.py"))]
-    #[cfg(test_environment = "ntfs")]
-    fn rules_ntfs(path: &Path) -> Result<()> {
         let snapshot = path.to_string_lossy().into_owned();
         let diagnostics = test_path(
             Path::new("flake8_executable").join(path).as_path(),
