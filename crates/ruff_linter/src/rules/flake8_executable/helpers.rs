8a7a8ebe2c36740a2a2f8f5c19a224f57cab2f53
Mike Foster <i.am.mike.foster@gmail.com>
2025-04-22 09:44:42 +0000

    improve comments to executable by default

--- crates/ruff_linter/src/rules/flake8_executable/helpers.rs
+++ crates/ruff_linter/src/rules/flake8_executable/helpers.rs
@@ -19,15 +19,16 @@ pub(super) fn is_executable(filepath: &Path) -> Result<bool> {
 // See #3110, #5445, #10084, #12941
 //
 // Benchmarking shows no noticable difference in performance if we run this check on
-// all systems, as long as we use a `OnceLock`.
+// all systems, as long as we use a `OnceLock` and a simple test first (filemode of pyproject.toml).
 static EXECUTABLE_BY_DEFAULT: OnceLock<bool> = OnceLock::new();
 
 pub(crate) fn executable_by_default(settings: &LinterSettings) -> bool {
     *EXECUTABLE_BY_DEFAULT.get_or_init(|| {
-        is_executable(&settings.project_root.join("pyproject.toml")).unwrap_or(true) &&
-        NamedTempFile::new_in(&settings.project_root)
-            .map_err(std::convert::Into::into)
-            .and_then(|tmpfile| is_executable(tmpfile.path()))
-            .unwrap_or(false) // Assume a normal filesystem in case of read-only, IOError, ...
+        is_executable(&settings.project_root.join("pyproject.toml")).unwrap_or(true)
+            // if pyproject.toml is executable OR doesn't exist, run a slower check too:
+            && NamedTempFile::new_in(&settings.project_root)
+                .map_err(std::convert::Into::into)
+                .and_then(|tmpfile| is_executable(tmpfile.path()))
+                .unwrap_or(false) // Assume a normal filesystem in case of read-only, IOError, ...
     })
 }
